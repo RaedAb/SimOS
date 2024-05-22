61817b6a3b560e62a486effbf4c15c7d9a6ac7c0
Raed Abuzaid <abuzaidraed@gmail.com>
2024-05-22 18:34:48 -0400

    added main

---
+++ test_driver/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "../include/SimOS.h"
+
+int main()
+{
+    std::cout << "Sim OS Compiled Successfully" << std::endl;
+    /**
+     * Testing interface coming soon
+    */
+}
